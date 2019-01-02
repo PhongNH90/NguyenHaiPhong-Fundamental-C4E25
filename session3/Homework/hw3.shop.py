c8165e2a94a6d5a671f26ab921fd8c7c8964327c
PhongNH90 <phong.nh@ubgroup.vn>
2019-01-02 16:48:14 +0700

    initial commit

--- session3/Homework/hw3.shop.py
+++ session3/Homework/hw3.shop.py
@@ -1,18 +1,29 @@
-items = ["T-shirt", "Sweater"]
-x = "Welcome to our shop, what do you want?"
+x = input("Welcome to our shop, what do you want? (C, R, U, D) ")
 print(x)
-print("Our items: ", items)
 
-# y = input("Enter your items: ")
-# items.append(y)
-# print("Our items: ", items)
+items = ["T-shirt", "Sweater"]
 
+while True:
+    if x == "R":
+        print("Our items: ", items)
+        x = input("Welcome to our shop, what do you want? (C, R, U, D) ")
 
-z = int(input("Update postion? "))
-a = input("New item: ")
-items[z] = a
-print("Our items: ", items)
+    elif x == "C":
+        y = input("Enter your items: ")
+        items.append(y)
+        print("Our items: ", items)
+        x = input("Welcome to our shop, what do you want? (C, R, U, D) ")
 
-re = int(input("Delete position: "))
-items.pop(re)
-print("Our items: ", items)
+    elif x == "U":
+        z = int(input("Update postion? "))
+        a = input("New item: ")
+        items[z] = a
+        print("Our items: ", items)
+        x = input("Welcome to our shop, what do you want? (C, R, U, D) ")
+
+    elif x == "D":
+        re = int(input("Delete position: "))
+        items.pop(re)
+        print("Our items: ", items)
+        x = input("Welcome to our shop, what do you want? (C, R, U, D) ")
+
